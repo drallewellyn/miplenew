6b9d10591e84bcef9b7f61a96e89d5c34fc18a2a
Anthony LLewellyn <drallewellyn@me.com>
2015-10-17 00:02:02 +1100

    separate out tagging for pletools and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "pages#home"
-
+  root 'pages#home'
+  get 'pages/home'
   resources :pletools
   resources :posts
   get 'tags/:tag', to: 'posts#index', as: :tag
+  get 'tags/:tag', to: 'pletools#index', as: :pletag
 end
