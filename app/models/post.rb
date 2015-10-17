dfa6bfc1349a88b9152c29ed6a51db35c152ec3d
Anthony Llewellyn <drallewellyn@me.com>
2015-10-17 20:58:28 +1100

    add image validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   acts_as_taggable
   acts_as_taggable_on :tags
   has_attached_file :image, :styles => { :large=> "900x900>", :medium => "300x300>", :thumb => "100x100>" }
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/gif", "image/png"]
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
   extend FriendlyId
