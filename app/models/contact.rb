af7c28f5354185cd62ae5ed9b1971fc1da81537b
Anthony Llewellyn <drallewellyn@me.com>
2015-10-19 20:14:34 -0700

    revise sendgrid email

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,7 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "New Contact",
-      :to => "drallewellyn@me",
+      :to => "drallewellyn@me.com",
       :from => %("#{name}" <#{email}>)
     }
   end
