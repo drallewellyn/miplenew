db701aac1d7f94b2a20f0e7a526f4cfdb8a9c2ef
Anthony Llewellyn <drallewellyn@me.com>
2015-10-20 23:35:21 -0700

    post and tool layout stylings

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   acts_as_taggable_on :tags
-  has_attached_file :image, :styles => { :large=> "900x900>", :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { :large=> "900x900>", :medium => "400x400>", :thumb => "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
