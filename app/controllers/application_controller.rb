debb0034ad6186b1ea79545b95725bd76c80c1b0
Anthony Llewellyn <drallewellyn@me.com>
2015-11-02 12:35:45 +1100

    style up rest of latest posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-before_filter :the_latest
+  before_filter :the_latest 
 
   private
 
   def the_latest
     @posts = Post.all.limit(8).order("created_at desc")
   end
-
 
 end
